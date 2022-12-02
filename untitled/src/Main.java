3e127ca21785283722b224356ef65f565265258a
”Ivan <test.email@gmail.com>
2022-12-02 19:25:28 +0300

    testing git

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello world from git!");
+        System.out.println("Hello1 world from git!");
     }
 }
